2d623d9680386ebc7f664a3f5407686b762538cf
WillGiab <2410250284@qq.com>
2023-11-07 17:45:52 +0800

    卡牌ui界面替换，美术素材添加

--- source/controller/entities/card.gd
+++ source/controller/entities/card.gd
@@ -5,3 +5,9 @@ var card_data : CardModel
 
 func _init(card_id: StringName) -> void:
 	card_data = CardModel.new(card_id)
+
+func needs_target() -> bool:
+	return false
+
+func can_release() -> bool:
+	return false
