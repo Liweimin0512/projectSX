a097f4819b3f08356f05899adc8db1e0a705b3d6
jeremyLee0512 <2410250284@qq.com>
2023-12-16 22:10:01 +0800

    音乐音效开发

---
+++ source/auto/audio_manager.gd
@@ -0,0 +1,10 @@
+extends Node
+
+var click_audios : Array =[
+	preload("res://asserts/audio/click1.ogg"),
+	preload("res://asserts/audio/click2.ogg"),
+	preload("res://asserts/audio/click3.ogg"),
+	preload("res://asserts/audio/click4.ogg"),
+	preload("res://asserts/audio/click5.ogg"),
+]
+
